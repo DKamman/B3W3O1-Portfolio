90c7a34ebfd4d112fc470f370c35d135d0ac3211
Unknown <99053978@mydavinci.nl>
2019-06-18 23:16:02 +0200

    commit

--- contact.php
+++ contact.php
@@ -16,8 +16,13 @@
   <body>
     <?php include "inc/nav.html" ?>
 
-    <div class="container-fluid" style="height: 2000px;">
-      <p>wtf is this shit</p>
+    <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="..." alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
     </div>
 
     <?php include "inc/footer.html" ?>
