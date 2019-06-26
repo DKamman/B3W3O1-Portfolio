e2d00ee487ce10aa086aed91c8be6bb7dbeddacf
Danny <99053978@mydavinci.nl>
2019-06-26 23:29:14 +0200

    commit

--- portfolio.php
+++ portfolio.php
@@ -57,7 +57,7 @@
 
         <div class="col-sm">
           <div class="card mx-auto" style="width: 18rem;">
-            <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+            <img class="card-img-top" src=img/portfolio/planningtool.png alt="Card image cap">
             <div class="card-body">
               <h5 class="card-title">Card title</h5>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
